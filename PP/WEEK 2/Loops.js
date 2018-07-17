b7e907b2b2d0b4dcba0a50ed70535e963a1c89a5
BIT Student <classroom@bgit.rs>
2018-07-17 16:25:48 +0200

    weekly project

--- PP/WEEK 2/Loops.js
+++ PP/WEEK 2/Loops.js
@@ -68,3 +68,49 @@ for(var i = 0; i <= 20; i++){
 }
 
 console.log(sum);
+
+/* Write a program that computes average marks of the following students. Then use this average to determine the corresponding grade.  */
+
+var students = [
+    ['David', 80],
+    ['Marko', 77],
+    ['Dany', 88],
+    ['John', 95],
+    ['Thomas', 68]
+];
+
+var gradeSum = 0;
+
+for(var i = 0; i < students.length; i++){
+    gradeSum += students[i][1];
+}
+
+var averGrade = gradeSum / students.length;
+console.log(averGrade);
+
+if (averGrade < 60 ) {
+    console.log('Average grade is F.');
+} else if( averGrade < 70){
+    console.log('Average grade is D');
+}else if( averGrade < 80){
+    console.log('Average grade is C');
+}else if( averGrade < 90){
+    console.log('Average grade is B');
+}else if( averGrade < 100){
+    console.log('Average grade is A');
+}
+
+/* Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
+
+for(var i =0; i <= 100; i++){
+    if(i % 5 == 0 && i % 3 == 0){
+        console.log('FizzBuzz');
+    }else if(i % 3 == 0 ){
+        console.log('Fizz');
+    }else if(i % 5 == 0){
+        console.log('Buzz');
+    }else{
+        console.log(i)
+    }
+}
+
